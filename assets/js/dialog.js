5da3173788a467dac726f10d213233eaa9fbf5cf
Cody-Macedo <macedo.cody@gmail.com>
2021-12-14 14:23:34 +0100

    add source

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -6,8 +6,18 @@ const btnClose = document.getElementsByClassName("close")[0];
 
 const btnOpen = function (data) {
     modal.style.display = "flex";
+    const audio = document.getElementById('audio');
+    const source = document.getElementById('audioSource');
+    source.src = data[0].file
+    // audio.load(); //call this to just preload the audio without playing
+    // audio.play(); //call this to play the song right away
+
+    const count = Object.keys(data).length;
+    console.log(count);
     data.forEach((file) => {
         console.log(file.file)
+        const element = document.getElementById("test");
+        element.innerHTML = "<p>" + file.file + "</p>";
     });
 }
 
