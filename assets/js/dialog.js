af4ff236b6e57585facdb880f01a00a776c13ac9
Cody-Macedo <macedo.cody@gmail.com>
2021-12-14 13:44:42 +0100

    add style / Multiple modal in progress

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -1,18 +1,21 @@
 const modal = document.getElementById("modal");
 
-const btnOpen = document.getElementById("btnOpen");
+// const btnOpen = document.getElementById("btnOpen");
 
 const btnClose = document.getElementsByClassName("close")[0];
 
-btnOpen.onclick = function() {
+const btnOpen = function (data) {
     modal.style.display = "flex";
+    data.forEach((file) => {
+        console.log(file.file)
+    });
 }
 
-btnClose.onclick = function() {
+btnClose.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
