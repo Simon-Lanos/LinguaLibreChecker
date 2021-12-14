ab37b4a1caf0dbe46ec63aa7011850e9ba1a57e4
Simon LANOS <simon.lanos@siqual.fr>
2021-12-14 09:33:11 +0100

    feat: add data.js with list of files and metadata

---
+++ assets/js/data.js
@@ -0,0 +1,42 @@
+const data = [
+    {
+        author: 'Adélaïde Calais',
+        date: '13 décembre 2021 à 08:16',
+        duration: 1.1,
+        size: 99,
+        file: './assets/audio/LL-Q150_(fra)-Adélaïde_Calais_WMFr-jeune.wav',
+        tags: [],
+    },
+    {
+        author: 'Adélaïde Calais',
+        date: '13 décembre 2021 à 08:16',
+        duration: 1.0,
+        size: 91,
+        file: './assets/audio/LL-Q150_(fra)-Adélaïde_Calais_WMFr-roc.wav',
+        tags: [],
+    },
+    {
+        author: 'Adélaïde Calais',
+        date: '13 décembre 2021 à 08:16',
+        duration: 1.3,
+        size: 123,
+        file: './assets/audio/LL-Q150_(fra)-Adélaïde_Calais_WMFr-ras.wav',
+        tags: [],
+    },
+    {
+        author: 'Adélaïde Calais',
+        date: '13 décembre 2021 à 08:16',
+        duration: 1.0,
+        size: 91,
+        file: './assets/audio/LL-Q150_(fra)-Adélaïde_Calais_WMFr-fou_à_lier.wav',
+        tags: [],
+    },
+    {
+        author: 'Adélaïde Calais',
+        date: '13 décembre 2021 à 08:17',
+        duration: 1.4,
+        size: 131,
+        file: './assets/audio/LL-Q150_(fra)-Adélaïde_Calais_WMFr-rat.wav',
+        tags: [],
+    },
+];
