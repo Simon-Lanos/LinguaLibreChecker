a6b3e62831de15203067f911bd9b5e05afdce4a4
Simon LANOS <simon.lanos@siqual.fr>
2021-12-15 11:22:44 +0100

    feat: add translation keys and use the tranlation function in table rendering

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -2,19 +2,25 @@ const translate = function (key, domain = navigator.language) {
     const translations = {
         fr: {
             'author': 'Auteur',
+            'action': 'Action',
             'date': 'Date',
             'duration': 'Durée',
             'export': 'Exporter',
             'file': 'Fichier',
             'size': 'Taille',
+            'tags': 'Tags',
+            'word': 'Mot',
         },
         en: {
             'author': 'Author',
+            'action': 'Action',
             'date': 'Date',
             'duration': 'Duration',
             'export': 'Export',
             'file': 'File',
             'size': 'Size',
+            'tags': 'Tags',
+            'word': 'Word',
         }
     };
 
